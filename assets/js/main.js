99e08e757b2f7cdf11a5c5e7b2d8b9cd11349803
Noelia Muñiz <nmunizmenend@gmail.com>
2018-07-12 22:32:39 +0200

    Finalizando la práctica sin apartado de Task List

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 import { FormContact } from './form-contact.js'
+
 export class Main {
 
     constructor() {
@@ -10,16 +11,57 @@ export class Main {
         this.aSections = document.querySelectorAll("section")
         this.oOffsets = []
 
+        /**Links del menu-top y objectos sections */
+        this.olinkPortada= document.querySelector('#lportada')
+        this.oSeccionPortada = document.querySelector('#portada')
+        this.olinkQuienSoy = document.querySelector('#lquiensoy')
+        this.oSeccionQuienSoy = document.querySelector('#quiensoy')
+        this.olinkestudios = document.querySelector('#lestudios')
+        this.oSeccionestudios = document.querySelector('#estudios')
+        this.olinkexperiencia = document.querySelector('#lexperiencia')
+        this.oSeccionexperiencia = document.querySelector('#experiencia')
+        this.olinksobremi = document.querySelector('#lsobremi')
+        this.oSeccionsobremi = document.querySelector('#sobremi')
+        this.olinkcontacto = document.querySelector('#lcontacto')
+        this.oSeccioncontacto= document.querySelector('#contacto')
+
+        /**Links del menu-button */
+
+        this.olinkPortadaf=document.querySelector('#lportadaf')
+        this.olinkQuienSoyf = document.querySelector('#lquiensoyf')
+        this.olinkestudiosf = document.querySelector('#lestudiosf')
+        this.olinkexperienciaf = document.querySelector('#lexperienciaf')
+        this.olinksobremif = document.querySelector('#lsobremif')
+        this.olinkcontactof = document.querySelector('#lcontactof')
+
+
+
         //Formulario de contacto
 
         this.oFormContact = new FormContact()
 
-        //Prepara el array de offsets usado en el smooth scrooll
+        //Prepara el array de offsets usado en el sticky menu
         this.prepararNavegacion()
+
+
+
     }
 
     defineEventListeners() {
         this.oBtnMenu.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinkPortada.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinkQuienSoy.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinkestudios.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinkexperiencia.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinksobremi.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinkcontacto.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinkPortadaf.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinkQuienSoyf.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinkestudiosf.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinkexperienciaf.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinksobremif.addEventListener('click', this.smoothScroll.bind(this))
+        this.olinkcontactof.addEventListener('click', this.smoothScroll.bind(this))
+
         window.addEventListener('scroll', this.changeMenuStyle.bind(this))
     }
 
@@ -68,49 +110,62 @@ export class Main {
         return top;
     }
 
-    smoothScroll() {
+    smoothScroll(oEvent) {
+        let value = oEvent.target.id
+        let elm;
+        oEvent.preventDefault();
+
+        if (value == 'menuBtn') elm=this.oMenuBottom
+        if (value == 'lportada' || value == 'lportadaf') elm=this.oSeccionPortada
+        if (value == 'lquiensoy' || value =='lquiensoyf') elm=this.oSeccionQuienSoy
+        if (value == 'lestudios' || value == 'lestudiosf') elm=this.oSeccionestudios
+        if (value == 'lexperiencia' || value == 'lexperienciaf') elm=this.oSeccionexperiencia
+        if (value == 'lsobremi' || value == 'lsobremif') elm=this.oSeccionsobremi
+        if (value == 'lcontacto' || value == 'lcontactof') elm=this.oSeccioncontacto
+
         //Determina la posición actual a partir de la cual realizar el scroll
         let startY = window.pageYOffset;
-        console.log(startY);
-        let stopY = this.elmYPosition(this.oMenuBottom);
-        console.log(stopY);
-        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        let stopY = this.elmYPosition(elm);
+
+        let distance = stopY > startY ? stopY - startY : startY - stopY;
         if (distance < 100) {
             scrollTo(0, stopY);
             return;
         }
-        var speed = Math.round(distance / 100);
+        let speed = Math.round(distance / 100);
         if (speed >= 20) speed = 20;
-        var step = Math.round(distance / 25);
-        var leapY = stopY > startY ? startY + step : startY - step;
-        var timer = 0;
+        let step = Math.round(distance / 25);
+        let leapY = stopY > startY ? startY + step : startY - step;
+        let timer = 0;
         if (stopY > startY) {
-            for (var i = startY; i < stopY; i += step) {
+            for (let i = startY; i < stopY; i += step) {
                 setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
                 leapY += step;
                 if (leapY > stopY) leapY = stopY;
                 timer++;
             }
             return;
-        }
-        for (var i = startY; i > stopY; i -= step) {
+        } else {
+        for (let i = startY; i > stopY; i -= step) {
             setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
             leapY -= step;
             if (leapY < stopY) leapY = stopY;
             timer++;
         }
     }
+    }
 
-
+    /**Calcula el offsetY del elemento pasado como parámetro */
     elmYPosition(element) {
-        var y = element.offsetTop;
-        var node = element;
+        let y = element.offsetTop;
+        let node = element;
         while (node.offsetParent && node.offsetParent != document.body) {
             node = node.offsetParent;
             y += node.offsetTop;
         }
         return y;
     }
+
 
 
 }
